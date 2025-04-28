0ce12f04261fcc9e9f4cce8aaab231ce1261c2b0
AJISGit <209511801+AJISGit@users.noreply.github.com>
2025-04-28 14:26:09 -0500

    Now you can select the level from the command line

--- main.lua
+++ main.lua
@@ -2,14 +2,13 @@ local tilemap = require('tilemap')
 local file = nil
 local tiles = nil
 
-function love.load()
-    file = io.open('level.txt')
+function love.load(arg, uArg)
+
+    love.window.setTitle('Bullet-R.E.P.O.')
+
+    file = io.open(uArg[2] or 'level.txt')
     tiles = tilemap.generateTiles(file)
     io.close(file)
-    
-    for i, v in ipairs(tiles) do
-        print(v)
-    end
 
 end
 
