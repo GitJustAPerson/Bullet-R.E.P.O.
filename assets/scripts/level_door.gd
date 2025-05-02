8a87d27504fd4cff8bbb212977701c9f40923a3c
AJISGit <spacedaj@protonmail.com>
2025-05-02 16:39:18 -0500

    Now you can customize the destination position of the door

--- assets/scripts/level_door.gd
+++ assets/scripts/level_door.gd
@@ -6,17 +6,24 @@ extends Area2D
 @onready var level_manager: LevelManager = get_parent().get_parent().get_parent().get_node("LevelManager")
 
 @export var DestinationLevel: String;
+@export var StartingPositionNode: String;
 
 var has_entered = false;
 
 func _on_body_entered(body: Node2D):
-	if not has_entered:
-		has_entered = true;
-		
-		var new_level: Node = level_manager.load_level("res://assets/scenes/levels/" + DestinationLevel + ".tscn");
-		new_level.position = Vector2(0.0, 0.0);
-		get_parent().get_parent().get_parent().add_child(new_level)
-		get_parent().queue_free()
+	if body.name == 'Player':
+		if not has_entered:
+			has_entered = true;
+			
+			var new_level: Node = level_manager.load_level("res://assets/scenes/levels/" + DestinationLevel + ".tscn");
+			new_level.position = Vector2(0.0, 0.0);
+			get_parent().get_parent().get_parent().add_child(new_level)
+			
+			var starting_node: Node2D = new_level.get_node(StartingPositionNode)
+			
+			body.position = starting_node.position;
+			
+			get_parent().queue_free()
 
 func _ready() -> void:
 
